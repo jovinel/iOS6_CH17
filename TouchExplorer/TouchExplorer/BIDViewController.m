6ac609d2ea4b0ced58a3771a9bd1b0f4dffafc16
crescens <crescens.techstars@gmail.com>
2013-12-09 19:20:57 +0800

    Implemented a demo for touch detection and drag detection
    Signed-off-by: crescens <crescens.techstars@gmail.com>

--- TouchExplorer/TouchExplorer/BIDViewController.m
+++ TouchExplorer/TouchExplorer/BIDViewController.m
@@ -8,22 +8,43 @@
 
 #import "BIDViewController.h"
 
-@interface BIDViewController ()
-
-@end
 
 @implementation BIDViewController
 
-- (void)viewDidLoad
+- (void)updateLabelsFromTouches:(NSSet *)touches
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSUInteger numTaps = [[touches anyObject] tapCount];
+    NSString *tapsMessage = [[NSString alloc] initWithFormat:@"%d taps detected", numTaps];
+    self.tapsLabel.text = tapsMessage;
+    
+    NSUInteger numTouches = [touches count];
+    NSString *touchMsg = [[NSString alloc] initWithFormat:@"%d touches detected", numTouches];
+    self.touchesLabel.text = touchMsg;
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Touch Event Methods
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.messageLabel.text = @"Touches Began";
+    [self updateLabelsFromTouches:touches];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self.messageLabel.text = @"Touches Cancelled";
+    [self updateLabelsFromTouches:touches];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self.messageLabel.text = @"Touches Ended.";
+    [self updateLabelsFromTouches:touches];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self.messageLabel.text = @"Drag Detected";
+    [self updateLabelsFromTouches:touches];
 }
 
 @end
