47d32c03798b26dca8f17ca4fc9dae3098e87638
crescens <crescens.techstars@gmail.com>
2013-12-09 19:03:09 +0800

    First commit, initialize data and files for the project.
    Signed-off-by: crescens <crescens.techstars@gmail.com>

---
+++ TouchExplorer/TouchExplorerTests/TouchExplorerTests.m
@@ -0,0 +1,32 @@
+//
+//  TouchExplorerTests.m
+//  TouchExplorerTests
+//
+//  Created by Crescens Techstars on 12/9/13.
+//  Copyright (c) 2013 Crescens Techstars. All rights reserved.
+//
+
+#import "TouchExplorerTests.h"
+
+@implementation TouchExplorerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TouchExplorerTests");
+}
+
+@end
