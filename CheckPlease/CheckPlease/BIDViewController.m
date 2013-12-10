440d9e02e754854ee6406cc57078c558f289105e
crescens <crescens.techstars@gmail.com>
2013-12-10 13:10:28 +0800

    Finished the demo for detecting a checkmark using GestureRecognizer

--- CheckPlease/CheckPlease/BIDViewController.m
+++ CheckPlease/CheckPlease/BIDViewController.m
@@ -7,23 +7,34 @@
 //
 
 #import "BIDViewController.h"
+#import "BIDCheckMarkRecognizer.h"
 
-@interface BIDViewController ()
 
-@end
 
 @implementation BIDViewController
+
+- (void)doCheck:(BIDCheckMarkRecognizer *)check
+{
+    self.label.text = @"Checkmark";
+    [self performSelector:@selector(eraseLabel) withObject:nil afterDelay:1.6];
+    
+}
+
+- (void)eraseLabel
+{
+    self.label.text = @"";
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    BIDCheckMarkRecognizer *check = [[BIDCheckMarkRecognizer alloc] initWithTarget:self action:@selector(doCheck:)];
+    
+    [self.view addGestureRecognizer:check];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
