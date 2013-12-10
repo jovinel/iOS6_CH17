3276413e2ab5fe5aae6ebde561e24493aab9bea6
crescens <crescens.techstars@gmail.com>
2013-12-10 11:53:01 +0800

    Initialize data for the project and added two files to be used in the project, CGPointUtils.c and CGPointUtils.h

---
+++ CheckPlease/CheckPlease/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  CheckPlease
+//
+//  Created by Crescens Techstars on 12/10/13.
+//  Copyright (c) 2013 Crescens Techstars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
