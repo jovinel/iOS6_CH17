63d0e53e6ba2417c5832baebc31cdbd4891beb96
crescens <crescens.techstars@gmail.com>
2013-12-10 11:06:10 +0800

    Initiliaze data for the project and created four outlets for the four labels in the view.

---
+++ TapTaps/TapTaps/BIDViewController.h
@@ -0,0 +1,18 @@
+//
+//  BIDViewController.h
+//  TapTaps
+//
+//  Created by Crescens Techstars on 12/10/13.
+//  Copyright (c) 2013 Crescens Techstars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *singleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *doubleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tripleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *quadrupleLabel;
+
+@end
---
+++ TapTaps/TapTapsTests/TapTapsTests.h
@@ -0,0 +1,13 @@
+//
+//  TapTapsTests.h
+//  TapTapsTests
+//
+//  Created by Crescens Techstars on 12/10/13.
+//  Copyright (c) 2013 Crescens Techstars. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TapTapsTests : SenTestCase
+
+@end
