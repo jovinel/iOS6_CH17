b72d99e8b329d379657df685da2f10ddd824479f
crescens <crescens.techstars@gmail.com>
2013-12-10 10:12:31 +0800

    Initialize data for the project and created an Outlet for the label used in view.

---
+++ Swipes/SwipesTests/SwipesTests.m
@@ -0,0 +1,32 @@
+//
+//  SwipesTests.m
+//  SwipesTests
+//
+//  Created by Crescens Techstars on 12/10/13.
+//  Copyright (c) 2013 Crescens Techstars. All rights reserved.
+//
+
+#import "SwipesTests.h"
+
+@implementation SwipesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SwipesTests");
+}
+
+@end
